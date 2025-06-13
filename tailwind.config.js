240899b79fde0f5800d249e5d4956f5171a523d6
AbdouramaneIssa <ramanabdou507@gmail.com>
2025-06-13 06:34:06 +0100

    UPDATE

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...fontFamily.sans],
+        sans: ['Inter', 'sans-serif'],
       },
       colors: {
         primary: {
